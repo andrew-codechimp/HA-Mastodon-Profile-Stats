8c64a1da29164c79925d603454255e97042f45ca
Andrew Jackson <andrew@codechimp.org>
2023-07-19 13:21:05 +0000

    Remove unused entities

--- custom_components/mastodon_profile_stats/api.py
+++ custom_components/mastodon_profile_stats/api.py
@@ -44,15 +44,6 @@ class MastodonProfileStatsApiClient:
             method="get", url="https://jsonplaceholder.typicode.com/posts/1"
         )
 
-    async def async_set_title(self, value: str) -> any:
-        """Get data from the API."""
-        return await self._api_wrapper(
-            method="patch",
-            url="https://jsonplaceholder.typicode.com/posts/1",
-            data={"title": value},
-            headers={"Content-type": "application/json; charset=UTF-8"},
-        )
-
     async def _api_wrapper(
         self,
         method: str,
