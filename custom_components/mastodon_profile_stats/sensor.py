95e74d88255453c5e5e45090c117b52605956d33
Andrew Jackson <andrew@codechimp.org>
2023-07-21 14:07:05 +0000

    Change to real propert

--- custom_components/mastodon_profile_stats/sensor.py
+++ custom_components/mastodon_profile_stats/sensor.py
@@ -43,4 +43,4 @@ class MastodonProfileStatsSensor(MastodonProfileStatsEntity, SensorEntity):
     @property
     def native_value(self) -> str:
         """Return the native value of the sensor."""
-        return self.coordinator.data.get("body")
+        return self.coordinator.data.get("followers_count")
